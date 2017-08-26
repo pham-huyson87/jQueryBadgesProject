a6ae747f50c8fe5b07271e30f93a642c94da5fab
pham-huyson87 <pham.huyson87@hotmail.com>
2017-08-25 22:02:09 -0400

    jQuery Badge Project
    Add AJAX call to retrieve the badges and populate the page with them

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/user13373.json',
+        dataType: 'jsonp',
+        success: function (response)
+        {
+            var divBadges = $("#badges");
+
+            response
+                .courses.completed.forEach(function (course) {
+
+                var divCourse = $("<div class=\"course\"></div>");
+
+                // Build the Course
+                divCourse
+                    .append("<h3>" + course.title + "</h3>");
+
+                divCourse
+                    .append("<img src=\"" + course.badge + "\" />");
+
+                divCourse
+                    .append("<a href=\"" + course.url + "\" target=\"_blank\" class=\"btn btn-primary\">See Course</a>");
+
+
+                // Add the Course to the Badges
+                divCourse
+                    .appendTo(divBadges);
+            });
+
+
+        }
+    });
+
 
 });
